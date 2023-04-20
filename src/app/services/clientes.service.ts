c685f3b7aa10dde346bffbb0947ae4e7f7399f2d
Lucas Santos de Oliveira <lucas.santos003@outlook.com>
2023-04-20 20:31:26 -0300

    Começado tratamento de erros

--- src/app/services/clientes.service.ts
+++ src/app/services/clientes.service.ts
@@ -1,38 +1,47 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 import { Cliente } from '../models/Cliente.model';
+import { catchError, EMPTY, map, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClientesService {
-
   url = 'http://localhost:3000/clientes';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  create (cliente: Cliente){
-    return this.http.post(this.url, cliente);
+  create(cliente: Cliente) {
+    return this.http.post(this.url, cliente)
   }
 
-  getAll (){
-    return this.http.get(this.url);
+  getAll(): Observable<Cliente[]> {
+    return this.http.get<Cliente[]>(this.url).pipe(
+      map((retorno) => retorno),
+      catchError((erro) => this.exibirErro(erro))
+    );
   }
 
-  getOne (id:number){
+  getOne(id: number) {
     // return this.http.get(this.url + '/' + id);
     return this.http.get(`${this.url}/${id}`);
   }
 
-  update (cliente: Cliente){
+  update(cliente: Cliente) {
     return this.http.put(`${this.url}/${cliente.id}`, cliente);
   }
 
-  delete (id: number){
+  delete(id: number) {
     return this.http.delete(`${this.url}/${id}`);
   }
 
-  login (){}
+  login() {}
 
-  logout (){}
+  logout() {}
+
+  exibirErro(erro: any): Observable<any> {
+    alert('Deu erro!');
+    console.log(erro);
+    return EMPTY;
+  }
 }
