6b92ed7c6d0f9845463661b07b4c09aa80ced178
Danilo Santos <santosdanilo2024@gmail.com>
2023-05-02 22:20:47 -0300

    Feita as validacoes do cliente e criada pagina create-produto

--- src/app/create-cliente/create-cliente.page.ts
+++ src/app/create-cliente/create-cliente.page.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { Cliente } from '../models/Cliente.model';
+import { ClientesService } from '../services/clientes.service';
 
 @Component({
   selector: 'app-create-cliente',
   templateUrl: './create-cliente.page.html',
   styleUrls: ['./create-cliente.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, Router]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class CreateClientePage implements OnInit {
 
@@ -18,9 +20,27 @@ export class CreateClientePage implements OnInit {
   senha = '';
   confirmeSenha ='';
 
-  constructor(private route: Router) { }
+  constructor(private router: Router, private clientesService: ClientesService) { }
 
   ngOnInit() {
   }
+
+  salvar(){
+    if(this.senha === this.confirmeSenha){
+      const cliente: Cliente = {
+        nome: this.nome,
+        email: this.email,
+        senha: this.senha
+      }
+      this.clientesService.create(cliente).subscribe(dados => {
+        alert("Cliente inserido com sucesso" + dados.id)
+        this.router.navigateByUrl('/home');
+      })
+    }else{
+      alert("Senhas nao conferem!")
+    }
+  }
+
+  
 
 }
