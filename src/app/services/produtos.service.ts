fc71fa1706aaccfdd43d78c76605d1ffd4c3bac3
Danilo Santos <santosdanilo2024@gmail.com>
2023-05-09 21:37:51 -0300

    Adicionado alert de erro no serviço de produtos

--- src/app/services/produtos.service.ts
+++ src/app/services/produtos.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Produto } from '../models/Produto.Model';
 import { HttpClient } from '@angular/common/http';
-import { EMPTY, Observable } from 'rxjs';
+import { catchError, EMPTY, map, Observable } from 'rxjs';
+
+import { AlertController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -9,30 +11,58 @@ import { EMPTY, Observable } from 'rxjs';
 export class ProdutosService {
 
   url = 'http://localhost:3000/produtos';
-
-  constructor(private http: HttpClient) { }
+ 
+  constructor(private http: HttpClient, private alertController: AlertController ) { }
 
   create (produto: Produto): Observable<Produto>{
-    return this.http.post<Produto>(this.url, produto);
+    return this.http.post<Produto>(this.url, produto).pipe(
+      map(retorno => retorno),
+      catchError(erro => this.exibirErro(erro))
+    );
   }
 
   getAll (): Observable<Produto[]>{
-    return this.http.get<Produto[]>(this.url);
+    return this.http.get<Produto[]>(this.url).pipe(
+      map(retorno => retorno),
+      catchError(erro => this.exibirErro(erro))
+    );
   }
 
   getOne (id: number): Observable<Produto>{
    // return this.http.get(this.url + '/' + id);
-    return this.http.get<Produto>(`${this.url}/${id}`);
+    return this.http.get<Produto>(`${this.url}/${id}`).pipe(
+      map(retorno => retorno),
+      catchError(erro => this.exibirErro(erro))
+    );
   }
 
   update (produto: Produto): Observable<Produto>{
-    return this.http.put<Produto>(`${this.url}/${produto.id}`, produto);
+    return this.http.put<Produto>(`${this.url}/${produto.id}`, produto).pipe(
+      map(retorno => retorno),
+      catchError(erro => this.exibirErro(erro))
+    );
   }
 
   delete (id: number){
     return this.http.delete(`${this.url}/${id}`);
   }
 
+  exibirErro(erro: any):Observable<any>{
+    const titulo = `Erro na conexão!`;
+    const msg = `verifique sua conexão \n ou \n Informe esse erro ao suporte ${erro.status}`; 
+    this.presentAlert(titulo, msg);
+    return EMPTY;
+  }
+
+  async presentAlert(titulo: string, msg: string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: msg,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
 
 
 }
